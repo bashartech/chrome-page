23378aabf0a0766c4d84d5d80c48a020462f2887
Bashar Sheikh <144168218+bashartech@users.noreply.github.com>
2024-11-04 01:38:08 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import HomePage from "@/app/component/Home/Home";
 
 export default function Home() {
   return (
+        <title>Bashar's Chrome</title>
+
     <>
     <div className="bg-[#1a1a1a] h-screen">
     <HomePage/>
